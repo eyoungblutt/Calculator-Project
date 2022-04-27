930538ba6df7473d9548d695e79ab73f8deeb977
Erin Youngblutt <eyoungblutt@example.com>
2022-04-27 21:47:30 +1000

    added js for numbers 1-9 to appear in textarea

--- Calculator Project/calculator.js
+++ Calculator Project/calculator.js
@@ -1,5 +1,51 @@
-// const buttonSeven = document.getElementById("botton-7");
+console.log(document.getElementById("button-7").innerText);
 
-// console.log(buttonSeven.innerText);
+const numberDisplay7 = () => {
+  document.getElementById("number-display").innerText = 7;
+};
+document.getElementById("button-7").addEventListener("click", numberDisplay7);
 
-// // have clicked button appear in id number-display
+const numberDisplay0 = () => {
+  document.getElementById("number-display").innerText = 0;
+};
+document.getElementById("button-0").addEventListener("click", numberDisplay0);
+
+const numberDisplay1 = () => {
+  document.getElementById("number-display").innerText = 1;
+};
+document.getElementById("button-1").addEventListener("click", numberDisplay1);
+
+const numberDisplay2 = () => {
+  document.getElementById("number-display").innerText = 2;
+};
+document.getElementById("button-2").addEventListener("click", numberDisplay2);
+
+const numberDisplay3 = () => {
+  document.getElementById("number-display").innerText = 3;
+};
+document.getElementById("button-3").addEventListener("click", numberDisplay3);
+
+const numberDisplay4 = () => {
+  document.getElementById("number-display").innerText = 4;
+};
+document.getElementById("button-4").addEventListener("click", numberDisplay4);
+
+const numberDisplay5 = () => {
+  document.getElementById("number-display").innerText = 5;
+};
+document.getElementById("button-5").addEventListener("click", numberDisplay5);
+
+const numberDisplay6 = () => {
+  document.getElementById("number-display").innerText = 6;
+};
+document.getElementById("button-6").addEventListener("click", numberDisplay6);
+
+const numberDisplay8 = () => {
+  document.getElementById("number-display").innerText = 8;
+};
+document.getElementById("button-8").addEventListener("click", numberDisplay8);
+
+const numberDisplay9 = () => {
+  document.getElementById("number-display").innerText = 9;
+};
+document.getElementById("button-9").addEventListener("click", numberDisplay9);
